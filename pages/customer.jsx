a79f6daf3c90318bad42a20275f004846bb5e0b7
HassanShanJava <hassanshan675@gmail.com>
2023-02-16 00:38:35 +0500

    worked on customer page

--- pages/customer.jsx
+++ pages/customer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BsPersonFill, BsThreeDotsVertical } from "react-icons/bs";
 
 const customer = () => {
   return (
-    <div>customer</div>
-  )
-}
+    <div>
+      <div>
+        <h2>Customers</h2>
+        <h2>Welcome Back, Hassan</h2>
+      </div>
+    </div>
+  );
+};
 
-export default customer
+export default customer;
